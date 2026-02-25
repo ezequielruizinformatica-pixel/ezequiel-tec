97cf8fe5565b4da7b51d6fe27e54bf41b5626d44
ezequielruizinformatica-pixel <ezequielruizinformatica@gmail.com>
2026-02-25 10:51:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function mostrarVisitante() {
 function voltar() {
   document.getElementById("aluno").classList.add("escondido");
   document.getElementById("visitante").classList.add("escondido");
-  document.getElementById("foto").classList.add("escondido");
   document.getElementById("inicio").classList.remove("escondido");
 }
 
@@ -27,38 +26,6 @@ function voltarAutomatico() {
 }
 
 let stream;
-
-function abrirFoto() {
-  trocarTela("inicio", "foto");
-
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(s => {
-      stream = s;
-      document.getElementById("camera").srcObject = stream;
-    });
-}
-
-function capturar() {
-  const video = document.getElementById("camera");
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-
-  ctx.drawImage(video, 0, 0);
-
-  const imagem = canvas.toDataURL("image/png");
-  document.getElementById("preview").src = imagem;
-
-  if (stream) {
-    stream.getTracks().forEach(track => track.stop());
-  }
-
-  gerarQR(imagem);
-}
-
-function gerarQR(dado) {
   console.log("Gerando QR...");
   document.getElementById("qrcode").innerHTML = "";
   new QRCode(document.getElementById("qrcode"), dado);
