60622e7ab9b162ac03572b16b0623646f6ee1271
ezequielruizinformatica-pixel <ezequielruizinformatica@gmail.com>
2026-02-26 15:52:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function mostrarTela(telaId) {
+function mostrarAluno() {
+    document.getElementById("inicio").classList.add("escondido");
+    document.getElementById("visitante").classList.add("escondido");
+    document.getElementById("aluno").classList.remove("escondido");
+}
+
+function mostrarVisitante() {
     document.getElementById("inicio").classList.add("escondido");
     document.getElementById("aluno").classList.add("escondido");
-    document.getElementById("visitante").classList.add("escondido");
+    document.getElementById("visitante").classList.remove("escondido");
+}
 
-    document.getElementById(telaId).classList.remove("escondido");
+function voltar() {
+    document.getElementById("aluno").classList.add("escondido");
+    document.getElementById("visitante").classList.add("escondido");
+    document.getElementById("inicio").classList.remove("escondido");
 }
