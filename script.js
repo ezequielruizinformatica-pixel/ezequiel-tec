45ef56fb2a35ffc4da352f731008dddd7ed243c4
ezequielruizinformatica-pixel <ezequielruizinformatica@gmail.com>
2026-03-09 11:19:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,67 @@
-function mostrarAluno() {
+function esconderTodas() {
     document.getElementById("inicio").classList.add("escondido");
+    document.getElementById("aluno").classList.add("escondido");
+    document.getElementById("materia").classList.add("escondido");
+    document.getElementById("turno").classList.add("escondido");
+    document.getElementById("professor").classList.add("escondido");
     document.getElementById("visitante").classList.add("escondido");
+    document.getElementById("mapa").classList.add("escondido");
+}
+
+function mostrarAluno() {
+    esconderTodas();
     document.getElementById("aluno").classList.remove("escondido");
 }
 
 function mostrarVisitante() {
-    document.getElementById("inicio").classList.add("escondido");
-    document.getElementById("aluno").classList.add("escondido");
+    esconderTodas();
     document.getElementById("visitante").classList.remove("escondido");
 }
 
 function voltar() {
-    document.getElementById("aluno").classList.add("escondido");
-    document.getElementById("visitante").classList.add("escondido");
+    esconderTodas();
     document.getElementById("inicio").classList.remove("escondido");
 }
 
 function mostrarMapa(titulo, imagem) {
-    document.getElementById("visitante").classList.add("escondido");
+    esconderTodas();
     document.getElementById("mapa").classList.remove("escondido");
 
     document.getElementById("titulo-mapa").innerText = titulo;
     document.getElementById("imagem-mapa").src = imagem;
 }
 
+function voltarParaVisitante() {
+    esconderTodas();
+    document.getElementById("visitante").classList.remove("escondido");
+}
+
 function irParaMateria() {
-    document.getElementById("aluno").classList.add("escondido");
+    esconderTodas();
     document.getElementById("materia").classList.remove("escondido");
 }
 
 function voltarParaAluno() {
-    document.getElementById("materia").classList.add("escondido");
+    esconderTodas();
     document.getElementById("aluno").classList.remove("escondido");
 }
 
 function irParaTurno() {
-    document.getElementById("materia").classList.add("escondido");
+    esconderTodas();
     document.getElementById("turno").classList.remove("escondido");
 }
 
 function voltarParaMateria() {
-    document.getElementById("turno").classList.add("escondido");
+    esconderTodas();
     document.getElementById("materia").classList.remove("escondido");
 }
 
 function irParaProfessor() {
-    document.getElementById("turno").classList.add("escondido");
+    esconderTodas();
     document.getElementById("professor").classList.remove("escondido");
 }
-function voltarParaVisitante() {
-    document.getElementById("mapa").classList.add("escondido");
-    document.getElementById("visitante").classList.remove("escondido");
+
+function voltarParaTurno() {
+    esconderTodas();
+    document.getElementById("turno").classList.remove("escondido");
 }
